fb4b65901ae4640d30a21813fb4dd763196643a0
박병욱 <quddnr2688@gmail.com>
2024-01-24 17:37:33 +0900

    알코도수 안내추가

--- reactCocktailWorkspace/src/components/ingredient/ requestIngredient/SelectBtn.jsx
+++ reactCocktailWorkspace/src/components/ingredient/ requestIngredient/SelectBtn.jsx
@@ -24,19 +24,24 @@ const StyledSelectBtnSpan = styled.span`
     }
 `;
 
-const SelectBtn = ({idx,name,no,btnActive,setState,setBtnActive}) => {
+const SelectBtn = ({setGuide,idx,name,no,btnActive,setState,setBtnActive}) => {
 
     const changeCategory = (e,no) => {
         if (e.target.innerText === btnActive) {
             // 클릭한 버튼이 이미 활성화된 상태일 때
             setState(""); // 상태 초기화
             setBtnActive(""); // btnActive 초기화
-          } else {
+        } else {
             // 클릭한 버튼이 비활성화된 상태일 때
             const listNo = no;
             setState(listNo);
             setBtnActive(e.target.innerText);
-          }
+        }
+        if(name === '술(약한 도수)'){
+            setGuide("술(약한 도수)는 40도 미만부터 업로드 가능합니다.");
+        }else if(name = '술(강한 도수)'){
+            setGuide("술(강한 도수)는 40도 이상부터 업로드 가능합니다.");
+        }
     }
 
     return (
